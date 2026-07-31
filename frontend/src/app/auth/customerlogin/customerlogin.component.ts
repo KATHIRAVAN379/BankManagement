cb6625cc046ed11d51bda20570056d187bb85ccd
KATHIRAVAN K <krithikroshan200@gmail.com>
2026-07-31 18:19:25 +0530

    updated budgets in angular.json and updated the frontend

--- frontend/src/app/auth/customerlogin/customerlogin.component.ts
+++ frontend/src/app/auth/customerlogin/customerlogin.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { AuthenService } from '../authen.service';
@@ -6,7 +7,7 @@ import { AuthenService } from '../authen.service';
 @Component({
   selector: 'app-customerlogin',
   standalone: true,
-  imports: [FormsModule,RouterLink],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './customerlogin.component.html',
   styleUrl: './customerlogin.component.css'
 })
@@ -14,6 +15,8 @@ export class CustomerloginComponent {
 
   accountNo: number = 0;
   password: string = "";
+  showPassword: boolean = false;
+  errorMessage: string = "";
 
   constructor(
     private auth: AuthenService,
@@ -21,31 +24,22 @@ export class CustomerloginComponent {
   ) { }
 
   login() {
-
+    this.errorMessage = "";
     const customer = {
       accountNo: this.accountNo,
       password: this.password
     };
 
     this.auth.customerLogin(customer).subscribe({
-
       next: (token) => {
-
         localStorage.setItem("token", token);
         localStorage.setItem("role", "ROLE_CUSTOMER");
-
         this.router.navigate(['/customerdashboard']);
-
       },
-
       error: () => {
-
-        alert("Invalid Account Number or Password");
-
+        this.errorMessage = "Invalid Account Number or Password. Please try again.";
       }
-
     });
-
   }
 
 }
